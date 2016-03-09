957afb0aa292124bdfeb26068e21888fef39a545
ACalza <albert.calzaretto@gmail.com>
2016-03-08 21:06:10 -0500

    Updated readme, and added some quick material-ui concepts

--- Nib/README.md
+++ Nib/README.md
@@ -1,2 +1,10 @@
 #Nib
 Nib is a reference managagement and citation generation tool.
+
+## Develop cycle
+
+npm build
+npm run start (Have it running in the background, not 100 percent neccessary)
+jpm run
+
+Make sure to build before you run if you edit a component
--- Nib/app/DropDownView/index.js
+++ Nib/app/DropDownView/index.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Toggle from 'material-ui/lib/toggle';
 
-class Account extends Component {
+
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  toggle: {
+    marginBottom: 16,
+  },
+};
+
+class DropDownView extends Component {
   render() {
-      return <p>Hello React </p>
+      return (
+        <div style={styles.block}>
+          <Toggle
+            label="Simple"
+            style={styles.toggle}
+          />
+          <Toggle
+            label="Toggled by default"
+            defaultToggled={true}
+            style={styles.toggle}
+          />
+          <Toggle
+            label="Disabled"
+            disabled={true}
+            style={styles.toggle}
+          />
+          <Toggle
+            label="Label on the right"
+            labelPosition="right"
+            style={styles.toggle}
+          />
+        </div>
+      )
   }
 }
 const container = document.getElementById('app')
-ReactDOM.render(<p>Hello react </p>, container)
+
+ReactDOM.render(<DropDownView /> ,container)
