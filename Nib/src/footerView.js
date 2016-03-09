8046e3d7d78b614f3e22a7a76da834fa70e70fe1
Zach Munro-Cape <zach.munrocape@gmail.com>
2016-03-09 11:04:01 -0500

    Dynamically set width of footer

--- Nib/src/footerView.js
+++ Nib/src/footerView.js
@@ -2,20 +2,21 @@
 const { Cu } = require('chrome');
 const panels = require("sdk/panel");
 const self = require("sdk/self");
+var utils = require('sdk/window/utils');
+
 
 exports.FooterView = class FooterView {
   constructor(button){
     this.button = button
     this.panel = panels.Panel({
-      width:1000,
       position:{bottom:0},
       contentURL: self.data.url('footer.html')
     })
   }
 
   show () {
-    this.panel.show({
-
-    })
+    var browserWindow = utils.getMostRecentBrowserWindow();
+    this.panel.width = browserWindow.content.innerWidth;
+    this.panel.show({});
   }
 }
