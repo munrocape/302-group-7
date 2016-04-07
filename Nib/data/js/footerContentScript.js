3aa5ac94cf7e4c73e698d2798a7f4b9d6ec1e1d8
Cody Howarth <codyhowarth@gmail.com>
2016-04-07 18:20:30 -0400

    Add more control flow to type of citation.

--- Nib/data/js/footerContentScript.js
+++ Nib/data/js/footerContentScript.js
@@ -7,24 +7,23 @@ self.port.on('wakeUp', function (value) {
 
 self.port.on(SHOW_BIB, function(bib, citation_style) {
     $("#bibliography").html("");
-    if (citation_style === 'IEEE')
-
     console.log(bib);
     sources = bib["sources"];
 
-    // Print sources according to citation_style
-    for (let i = 0; i < sources.length; i++) {
-        authors = parse_authors(sources[i]["authors"]);
-        citation = "<p>[" + (i + 1) + "] ";
-        citation = citation + authors + ", " "<span class = 'italicize'>" + sources[i]["name"] + "</span></p>"; //+ authors;
-        $("#bibliography").append(citation);
-    }
+    if (citation_style === 'IEEE') {
 
-    if (citation_style == 'MLA'){
+        // Print sources according to citation_style
+        for (let i = 0; i < sources.length; i++) {
+            authors = parse_authors(sources[i]["authors"]);
+            citation = "<p>[" + (i + 1) + "] ";
+            citation = citation + authors + ", " "<span class = 'italicize'>" + sources[i]["name"] + "</span></p>"; //+ authors;
+            $("#bibliography").append(citation);
+        }
+
+    } else if (citation_style == 'MLA'){
     	for (let i = 0; i < sources.length; i++) {
     		citation = mla_format(sources[i]);
     		$("#bibliography").append(citation);
-
     	}
     }
 
