9b48b837f34299fa3302d39843b06f4caf214330
Zach Munro-Cape <zach.munrocape@gmail.com>
2016-04-07 14:00:39 -0400

    report back what fields are present on a google books page

--- Nib/data/js/scraperContentScript.js
+++ Nib/data/js/scraperContentScript.js
@@ -13,32 +13,9 @@ function getEmptySource() {
 
 self.port.on(GOOGLE_BOOKS, function (url) {
 	console.log('in google book scraper');
-	//fields = $('#metadata_content_table').children[0].children;
-	var ref = getEmptySource();
-	// for(i = 0; i < fields.length; i++) {
-	// 	pair = fields[i].children
-	// 	key = pair[0];
-	// 	while(key.children.length > 0 && key.children != undefined) {
-	// 		key = key.children[0];
-	// 	}
-	// 	key = key.innerHTML;
-
-	// 	if (['Subjects', '&nbsp;', 'Export Citation'].indexOf(key) === -1) {
-	// 		key = key.toLowerCase();
-	// 		value = pair[1];
-	// 		while (value.children.length > 0 && value.children != undefined) {
-	// 			value = value.children[0];
-	// 		}
-	// 		value = value.innerHTML;
-	// 		console.log(key, value);
-	// 		if (key === 'title') {
-	// 			ref['title_of_source'] = value;
-	// 		} else {
-	// 			if (ref[key] != undefined) {
-	// 				ref[key] = value;
-	// 			}
-	// 		}
-	// 	}
-	// }
+	fields = $('#metadata_content_table').children[0].children;
+	ref
+	
+	for(i = 0; i < fields.length; i++) {pair = fields[i].children;key = pair[0];while(key.children.length > 0 && key.children != undefined) {key = key.children[0];}key = key.innerHTML;if (['Subjects', '&nbsp;', 'Export Citation'].indexOf(key) === -1) {key = key.toLowerCase();value = pair[1];while (value.children.length > 0 && value.children != undefined) {value = value.children[0];}value = value.innerHTML;self.port.emit('key_value_pair', {'key':key, 'value': value});}}
 	self.port.emit(SCRAPED_CITATION, ref);
 });
--- Nib/index.js
+++ Nib/index.js
@@ -14,16 +14,15 @@ let footerView = null;
 
 p = pageMod.PageMod({
   include: "https://books.google.ca/books?id=*",
-  contentScript: "self.port.emit('book_dom', document);",
+  contentScriptWhen: "ready",
+  contentScript: "console.log('in script');fields = document.getElementById('metadata_content_table').children[0].children;if(fields != null){for(i = 0; i < fields.length; i++) {pair = fields[i].children;key = pair[0];while(key.children.length > 0 && key.children != undefined) {key = key.children[0];}key = key.innerHTML;if (['Subjects', '&nbsp;', 'Export Citation'].indexOf(key) === -1) {key = key.toLowerCase();value = pair[1];while (value.children.length > 0 && value.children != undefined) {value = value.children[0];}value = value.innerHTML;self.port.emit('key_value_pair', [key, value]);}}}",
+  //contentScript: "fields = document.getElementById('metadata_content_table').children[0].children;",
   onAttach: function(worker) {
-      worker.port.on("book_dom", function(dom) {
-        console.log(dom);
-        dom = JSON.parse(dom);
-        console.log('got dom: '+ dom.childElementCount);
+      worker.port.on("key_value_pair", function(pair) {
+        console.log(pair[0] + ' ' + pair[1]);
       });
     }
 });
-
 ///
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
